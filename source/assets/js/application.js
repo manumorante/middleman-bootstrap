c46f3190e0ae586df77b1f5cc1b8bdaae5e2e3ba
Manu Morante <manu@iMac-de-Manu.local>
2014-11-18 00:09:18 +0100

    Organizing and refactor

--- source/assets/js/application.js
+++ source/assets/js/application.js
@@ -1,9 +1,7 @@
-/*
- * Lib (3rd Party libraries)
- *= require lib/_jquery-1.11.1.min
- *= require lib/_angular-1.2.26.min
- *= require lib/_bootstrap
- *
- * Modules (common JS in this project)
- *= require ui/nav
- */
+// Lib (3rd Party libraries)
+// = require lib/_jquery-1.11.1.min
+// = require lib/_angular-1.2.26.min
+// = require lib/_bootstrap
+
+// Modules (common JS in this project)
+// = require ui/nav
